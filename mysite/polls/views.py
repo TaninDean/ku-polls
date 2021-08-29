405d5d0f8f8faadb6cfa49bc9db8ca4836a75669
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-08-29 19:57:03 +0700

    done part5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
@@ -53,3 +54,9 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def get_queryset(self):
+    """
+    Excludes any questions that aren't published yet.
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
