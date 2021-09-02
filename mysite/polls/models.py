e5104fb3d8b8c9ae6bfdfc49dce8aca78351ff8b
Leodean <reiodean.dean@gmail.com>
2021-09-02 21:55:48 +0700

    fix code

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,20 +3,27 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pud_date = models.DateTimeField('date_published')
+    pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pud_date >= timezone().now - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    Choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+<<<<<<< HEAD
     def __str__(self) -> str:
         return self.Choice_text
+=======
+    def __str__(self):
+        return self.choice_text
+>>>>>>> be250ae (fix code)
