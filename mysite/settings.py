fb1b6e73a41274c57ec2eea1b5e9f3977a308d14
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-09-18 17:10:19 +0700

    make secret_key and debuh in .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d3gcu%gop6ue+$k8(%)i99n(g=zdv36!+c7=9#ik&2d5gcl(#7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
